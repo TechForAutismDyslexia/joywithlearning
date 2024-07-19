de97278fd177472cd29da5a0a1e5e585b4ffce35
Krishna Manoj PVR <pvrkmsbunny.com>
2024-07-19 13:32:23 +0530

    filezilla

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="d-flex flex-column flex-sm-row justify-content-evenly align-items-center footer text-center p-2">
-      <p className='m-1'>JoyWithLearning</p>
+      <p className='m-1'>Joy With Learning</p>
       <p className='m-1'>{`© 2024 KMIT. All Rights Reserved`}</p>
       <a href='mailto:info@joywithlearning.com' target='_blank' className='m-1'>info@joywithlearning.com</a>
     </div>
