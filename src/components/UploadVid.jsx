5d251afa1ace7dda7632e996183b6cef6af6f8b8
Krishna Manoj PVR <pvrkmsbunny.com>
2024-09-19 15:10:36 +0530

    p

--- src/components/UploadVid.jsx
+++ src/components/UploadVid.jsx
@@ -255,7 +255,7 @@ const UploadVid = () => {
                     Parent Phone No <span className="text-danger">*</span>
                   </label>
                 </div>
-                <select className="form-select mb-3" onChange={handleChange} name="PreferredCenter">
+                <select className="form-select mb-3" onChange={handleChange} name="preferredCenter">
                   <option selected>Preferred Center</option>
                   <option value="Barkathpura">Barkathpura</option>
                   <option value="Champapet">Champapet</option>
